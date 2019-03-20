ccf7451e09f6d3eddb30c3d0f836f46d25d10c67
pritamdattashuvo <48715735+pritamdattashuvo@users.noreply.github.com>
2019-03-20 11:13:01 +0600

    Add files via upload

---
+++ Abstraction/src/Abstraction.java
@@ -0,0 +1,13 @@
+public class Abstraction extends Student1{
+
+	public static void main(String args[]) {
+		
+	}
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
